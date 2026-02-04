236f4576b2634cdab7b07601f83997107532f360
Andreas <niessl.andreas@gmail.com>
2026-02-04 13:30:32 +0100

    Remove hash location routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
-import { useHashLocation } from "wouter/use-hash-location";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -9,12 +8,10 @@ import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
-    <WouterRouter hook={useHashLocation} base="/Bewerbungsseite">
-      <Switch>
-        <Route path="/" component={Home} />
-        <Route component={NotFound} />
-      </Switch>
-    </WouterRouter>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
